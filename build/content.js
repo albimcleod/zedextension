6a651f8ea9aa090671f7a23262bbd769921ac213
wisdom-weaver <danish.ansari.nox@gmail.com>
2021-10-27 20:49:26 +0530

    nak 34

--- build/content.js
+++ build/content.js
@@ -761,7 +761,8 @@ const loadHorses = () => {
 						})
 					}
 
-					distance_summary_div.innerHTML += '<br/>Have you donated recently?<br/>ETH: 0xc3422b8D7aa4be58C2BA7F07342620D5e13C2cD3<br/>Support: <a href="mailto:hello@stackednaks.com" target="_">hello@stackednaks.com</a>';
+					// distance_summary_div.innerHTML += '<br/>Have you donated recently?<br/>ETH: 0xc3422b8D7aa4be58C2BA7F07342620D5e13C2cD3<br/>Support: <a href="mailto:hello@stackednaks.com" target="_">hello@stackednaks.com</a>';
+					distance_summary_div.innerHTML += '<br/>Support: <a href="mailto:hello@stackednaks.com" target="_">hello@stackednaks.com</a>';
 
 					naks.appendChild(distance_summary_div);
 
